2efbbea34c4f458ff5cdc605c3bc69c9de9f82b8
misssbah07 <122712552+misssbah07@users.noreply.github.com>
2023-11-21 23:39:30 -0600

    Update App.js with useReducer

--- src/App.js
+++ src/App.js
@@ -1,81 +1,84 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-const initialTodos = [
-  {
-    title: 'Sample Todo',
-    description: 'This is a sample description.',
-    author: 'user1',
-    dateCreated: Date.now(),
-    complete: false,
-    dateCompleted: null,
-  },
-];
-
-function App() {
-  const [user, setUser] = useState(null);
-  const [todos, setTodos] = useState(initialTodos);
-  const [newTodo, setNewTodo] = useState({
+// Define your initial state and reducer function outside of the component
+const initialState = {
+  user: null,
+  todos: [
+    {
+      title: 'Sample Todo',
+      description: 'This is a sample description.',
+      author: 'user1',
+      dateCreated: Date.now(),
+      complete: false,
+      dateCompleted: null,
+    },
+  ],
+  newTodo: {
     title: '',
     description: '',
     author: '',
     dateCreated: null,
     complete: false,
     dateCompleted: null,
-  });
-  const [loginUser, setLoginUser] = useState('');
-  const [registerUser, setRegisterUser] = useState('');
+  },
+  loginUser: '',
+  registerUser: '',
+};
 
-  const handleLogin = () => {
-    setUser(loginUser);
-    setNewTodo((prevTodo) => ({ ...prevTodo, author: loginUser }));
-  };
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return { ...state, user: state.loginUser, newTodo: { ...state.newTodo, author: state.loginUser } };
+    case 'LOGOUT':
+      return { ...state, user: null };
+    case 'REGISTER':
+      return { ...state, user: state.registerUser, newTodo: { ...state.newTodo, author: state.registerUser } };
+    case 'ADD_TODO':
+      if (state.newTodo.title.trim() !== '') {
+        return {
+          ...state,
+          todos: [...state.todos, { ...state.newTodo, dateCreated: Date.now() }],
+          newTodo: { ...state.newTodo, title: '', description: '', dateCreated: null, complete: false, dateCompleted: null },
+        };
+      }
+      return state;
+    case 'TOGGLE_COMPLETE':
+      const updatedTodos = [...state.todos];
+      updatedTodos[action.index] = {
+        ...updatedTodos[action.index],
+        complete: !updatedTodos[action.index].complete,
+        dateCompleted: Date.now(),
+      };
+      return { ...state, todos: updatedTodos };
+    default:
+      return state;
+  }
+};
 
-  const handleRegister = () => {
-    setUser(registerUser);
-    setNewTodo((prevTodo) => ({ ...prevTodo, author: registerUser }));
-  };
-
-  const handleLogout = () => {
-    setUser(null);
-  };
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const handleAddTodo = () => {
-    if (newTodo.title.trim() !== '') {
-      setTodos([...todos, { ...newTodo, dateCreated: Date.now() }]);
-      setNewTodo({
-        title: '',
-        description: '',
-        author: user,
-        dateCreated: null,
-        complete: false,
-        dateCompleted: null,
-      });
-    }
+    dispatch({ type: 'ADD_TODO' });
   };
 
   const handleToggleComplete = (index) => {
-    const updatedTodos = [...todos];
-    updatedTodos[index] = {
-      ...updatedTodos[index],
-      complete: !updatedTodos[index].complete,
-      dateCompleted: Date.now(),
-    };
-    setTodos(updatedTodos);
+    dispatch({ type: 'TOGGLE_COMPLETE', index });
   };
 
   return (
     <div className="App">
-      {user ? (
+      {state.user ? (
         <div>
-          <h1>Welcome, {user}!</h1>
-          <button className="btn btn-danger" onClick={handleLogout}>
+          <h1>Welcome, {state.user}!</h1>
+          <button className="btn btn-danger" onClick={() => dispatch({ type: 'LOGOUT' })}>
             Logout
           </button>
           <h2>Todo List</h2>
           <ul className="list-group">
-            {todos.map((todo, index) => (
+            {state.todos.map((todo, index) => (
               <li key={index} className="list-group-item">
                 <input
                   type="checkbox"
@@ -95,16 +98,16 @@ function App() {
               <label>Title:</label>
               <input
                 type="text"
-                value={newTodo.title}
-                onChange={(e) => setNewTodo({ ...newTodo, title: e.target.value })}
+                value={state.newTodo.title}
+                onChange={(e) => dispatch({ type: 'CHANGE_TITLE', value: e.target.value })}
               />
             </div>
             <div>
               <label>Description:</label>
               <input
                 type="text"
-                value={newTodo.description}
-                onChange={(e) => setNewTodo({ ...newTodo, description: e.target.value })}
+                value={state.newTodo.description}
+                onChange={(e) => dispatch({ type: 'CHANGE_DESCRIPTION', value: e.target.value })}
               />
             </div>
             <button className="btn btn-success" onClick={handleAddTodo}>
@@ -120,11 +123,11 @@ function App() {
               <label>Username:</label>
               <input
                 type="text"
-                value={loginUser}
-                onChange={(e) => setLoginUser(e.target.value)}
+                value={state.loginUser}
+                onChange={(e) => dispatch({ type: 'CHANGE_LOGIN_USER', value: e.target.value })}
               />
             </div>
-            <button className="btn btn-primary" onClick={handleLogin}>
+            <button className="btn btn-primary" onClick={() => dispatch({ type: 'LOGIN' })}>
               Login
             </button>
           </div>
@@ -134,11 +137,11 @@ function App() {
               <label>Username:</label>
               <input
                 type="text"
-                value={registerUser}
-                onChange={(e) => setRegisterUser(e.target.value)}
+                value={state.registerUser}
+                onChange={(e) => dispatch({ type: 'CHANGE_REGISTER_USER', value: e.target.value })}
               />
             </div>
-            <button className="btn btn-success" onClick={handleRegister}>
+            <button className="btn btn-success" onClick={() => dispatch({ type: 'REGISTER' })}>
               Register
             </button>
           </div>
